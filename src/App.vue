d77779c4ccb1f1a00c45d288d1f4f19c0b348975
wangze0618 <2867273828@qq.com>
2022-01-20 20:22:19 +0800

    项目初始化

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,28 @@
 <template>
-  <div></div>
+  <div>
+    <Header></Header>
+    <div class="main">
+      <Goods></Goods>
+    </div>
+    <Footer></Footer>
+  </div>
 </template>
 
 <script>
-export default {};
+import Header from "./components/Header.vue";
+import Goods from "./components/Goods.vue";
+import Footer from "./components/Footer.vue";
+export default {
+  components: {
+    Header,
+    Goods,
+    Footer,
+  },
+};
 </script>
 
-<style>
+<style scoped>
+.main {
+  padding: 45px 0 0 0;
+}
 </style>
---
+++ src/components/Goods.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="my-goods-item">
+    <div class="left">
+      <div class="custom-control custom-checkbox">
+        <input type="checkbox" class="custom-control-input" id="input" />
+        <label class="custom-control-label" for="input">
+          <img
+            src="http://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"
+            alt=""
+          />
+        </label>
+      </div>
+    </div>
+    <div class="right">
+      <div class="top">商品名字</div>
+      <div class="bottom">
+        <span class="price">¥ 100</span>
+        <span><Count></Count></span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Count from "./Count.vue";
+export default {
+  components: {
+    Count,
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.my-goods-item {
+  display: flex;
+  padding: 10px;
+  border-bottom: 1px solid #ccc;
+  // margin: 43px 0 45px 0;
+  .left {
+    img {
+      width: 120px;
+      height: 120px;
+      margin-right: 8px;
+      border-radius: 10px;
+    }
+    .custom-control-label {
+      padding-left: 10px;
+    }
+    .custom-control-label::before,
+    .custom-control-label::after {
+      top: 50px;
+    }
+  }
+  .right {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    .top {
+      font-size: 14px;
+      font-weight: 700;
+    }
+    .bottom {
+      display: flex;
+      justify-content: space-between;
+      padding: 5px 0;
+      align-items: center;
+      .price {
+        color: red;
+        font-weight: bold;
+      }
+    }
+  }
+}
+</style>
